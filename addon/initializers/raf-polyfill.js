34925784cc664e7e43b884031618c8b924a73526
Chris Thoburn <runspired@gmail.com>
2015-10-28 12:59:36 -0500

    don't run the initialize code more than once, fixes #1

--- addon/initializers/raf-polyfill.js
+++ addon/initializers/raf-polyfill.js
@@ -2,10 +2,13 @@ import globalScope from '../utils/global-scope';
 import isFastboot from '../utils/is-fastboot';
 import setTimeoutOverride from '../utils/set-timeout-override';
 
+let hasInitialized = false;
+
 export function initialize(/* container, application */) {
-  if (!isFastboot()) {
+  if (!hasInitialized && !isFastboot()) {
     globalScope().AnimationFrame.shim();
     setTimeoutOverride();
+    hasInitialized = true;
   }
 }
 
