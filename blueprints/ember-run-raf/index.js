73e0255eaa1bcd861145cfdfbd7a0ae5733f6dc7
Chris Thoburn <runspired@gmail.com>
2015-11-06 17:16:18 -0600

    updates testing

--- blueprints/ember-run-raf/index.js
+++ blueprints/ember-run-raf/index.js
@@ -7,8 +7,7 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'animation-frame', target: '~0.2.4' },
-      { name: 'es5-shim', target: '~4.1.11'}
+      { name: 'animation-frame', target: '~0.2.4' }
     ]);
   }
 };
--- index.js
+++ index.js
@@ -6,9 +6,6 @@ module.exports = {
 
   included: function(app) {
     app.import(app.bowerDirectory + '/animation-frame/AnimationFrame.min.js');
-    if (app.env === 'test') {
-      app.import(app.bowerDirectory + '/es5-shim/es5-shim.js');
-    }
   },
 
   isDevelopingAddon: function() {
