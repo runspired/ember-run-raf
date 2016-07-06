d73c122fa8b06b19efb5e8d5c3864c82a7f36488
Scott H. Alexander <scotthalexander@gmail.com>
2016-07-06 11:46:48 -0700

    Prevent clearTimeout from throwing an illegal invocation TypeError
    ember-run-raf/utils/clear-timeout exports clearTimeout without binding
    it to the global scope, but ember-run-raf/utils/raf's clearFrame and
    ember-run-raf/utils/set-timeout-override's clearTimeout both call it
    without a context.

--- addon/utils/clear-timeout.js
+++ addon/utils/clear-timeout.js
@@ -1,3 +1,3 @@
 import globalScope from './global-scope';
-const nativeClearTimeout = globalScope.clearTimeout;
+const nativeClearTimeout = globalScope.clearTimeout.bind(globalScope);
 export default nativeClearTimeout;
