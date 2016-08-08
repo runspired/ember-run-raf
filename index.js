f5fa8397ac89196d9368b9ac5f855745d8ab3a64
Olufemi Adeojo <oadeojo@ciena.com>
2016-08-08 14:45:51 -0400

    fixed app.import to ensure ember-run-raf works in Ember engines

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ module.exports = {
   name: 'ember-run-raf',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/animation-frame/AnimationFrame.min.js');
+    if (typeof this.import === 'function' && !app.bowerDirectory) {
+      this.import('bower_components/animation-frame/AnimationFrame.min.js');
+    } else {
+      app.import(app.bowerDirectory + '/animation-frame/AnimationFrame.min.js');
+    }
   },
 
   isDevelopingAddon: function() {
