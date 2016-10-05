4e9fa2859dd9eb2129ad6d72fac37ab65a4fc0cd
Roxanne Panthaky <rpanthak@ciena.com>
2016-10-05 10:12:14 -0400

    refactor

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 /* jshint node: true */
 'use strict';
 
+
 module.exports = {
   name: 'ember-run-raf',
 
-  included: function() {
-    var app;
+  /**
+     This method climbs up the hierarchy of addons
+     up to the host application.
+     This prevents previous addons (prior to `this.import`, ca 2.7.0)
+     to break at importing assets when they are used nested in other addons.
+     @private
+     @method _findHost
+   */
+  _findHost: function() {
     var current = this;
+    var app;
+
     // Keep iterating upward until we don't have a grandparent.
     // Has to do this grandparent check because at some point we hit the project.
     do {
       app = current.app || app;
     } while (current.parent.parent && (current = current.parent));
+
+    return app;
+  },
+
+  included: function() {
+    var app = this._findHost();
 
     if (!app.bowerDirectory) {
       app.import('bower_components/animation-frame/AnimationFrame.min.js');
