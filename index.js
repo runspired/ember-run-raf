df3bf38db5788589d9a6a11c420b1c496ccfab2a
Ilya Radchenko <ilya@burstcreations.com>
2018-06-12 13:38:42 -0400

    fix: remove bower and blueprints
    No longer support IE9

--- index.js
+++ index.js
@@ -3,14 +3,6 @@
 module.exports = {
   name: 'ember-run-raf',
 
-  included: function(app) {
-    if (typeof this.import === 'function' && !app.bowerDirectory) {
-      this.import('bower_components/animation-frame/AnimationFrame.min.js');
-    } else {
-      app.import(app.bowerDirectory + '/animation-frame/AnimationFrame.min.js');
-    }
-  },
-
   isDevelopingAddon: function() {
     return false;
   }
